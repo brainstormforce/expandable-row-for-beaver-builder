f92ae274e1efcbe29ce5af0fffa32bab80a8ee21
Akash <akashd@bsf.io>
2016-12-01 15:43:18 +0530

    Remaining feature added

--- bb-expandable-row-module/bb-expandable-row-module.php
+++ bb-expandable-row-module/bb-expandable-row-module.php
@@ -47,13 +47,13 @@ function bb_er_row_extender( $form, $id ) {
 							'default'	=> 'none',
 							'toggle'	=> array(
 								'icon'	=> array(
-									'fields'	=> array('er_icon_size','er_icon_position','er_bc_icon','er_ac_icon','er_bc_icon_color','er_bc_icon_hcolor','er_ac_icon_color','er_ac_icon_hcolor','er_bc_bg_color','er_bc_bg_hcolor','er_ac_bg_color','er_ac_bg_hcolor')
+									'fields'	=> array('er_icon_size','er_icon_position','er_bc_icon','er_ac_icon','er_bc_icon_color','er_bc_icon_hcolor','er_ac_icon_color','er_ac_icon_hcolor')
 								),
 								'image'	=> array(
-									'fields'	=> array('er_image_size','er_bc_image','er_ac_image')
+									'fields'	=> array('er_image_size','er_icon_position','er_bc_image','er_ac_image')
 								)
 							),
-						),
+						),						
 						'er_image_size'	=> array(
 							'type'	=> 'text',
 							'label'	=> __( 'Image Size' ,''),
@@ -80,6 +80,23 @@ function bb_er_row_extender( $form, $id ) {
 								'right'	=> __( 'Right' ,''),
 							),
 							'default'	=> 'left',
+						),
+						'er_bg_type'	=> array(
+							'type'	=> 'select',
+							'label'	=> __( 'Background Type',''),
+							'options'	=> array(
+								'color'	=> __( 'Color' ,''),
+								'image'	=> __( 'Image' ,''),
+							),
+							'default'	=> 'color',
+							'toggle'	=> array(
+								'color'	=> array(
+									'fields'	=> array('er_bc_bg_color','er_bc_bg_hcolor','er_ac_bg_color','er_ac_bg_hcolor')
+								),
+								'image'	=> array(
+									'fields'	=> array('er_bc_bg_image','er_ac_bg_image')
+								),
+							),
 						),
 						'er_title_align'	=> array(
 							'type'	=> 'select',
@@ -137,7 +154,12 @@ function bb_er_row_extender( $form, $id ) {
 						),
 						'er_bc_image'	=> array(
 							'type'	=> 'photo',
-							'title'	=> __('Select Background Image',''),
+							'label'	=> __('Select Icon Image',''),
+							'show_remove'	=> true,
+						),
+						'er_bc_bg_image'	=> array(
+							'type'	=> 'photo',
+							'label'	=> __('Select Background Image',''),
 							'show_remove'	=> true,
 						),
 					)
@@ -186,7 +208,12 @@ function bb_er_row_extender( $form, $id ) {
 						),
 						'er_ac_image'	=> array(
 							'type'	=> 'photo',
-							'title'	=> __('Select Background Image',''),
+							'label'	=> __('Select Icon Image',''),
+							'show_remove'	=> true,
+						),
+						'er_ac_bg_image'	=> array(
+							'type'	=> 'photo',
+							'label'	=> __('Select Background Image',''),
 							'show_remove'	=> true,
 						),
 					)
@@ -274,9 +301,21 @@ function bb_er_row_css( $css, $nodes, $global_settings ) {
 		?>
 			<?php if ( $row->settings->is_enable == 'yes' ): ?>
 				.fl-node-<?php echo $row->node; ?> .bber-icon {
-					padding: 0 10px;
-					font-size: ;
+					color: <?php echo ($row->settings->er_bc_icon_color != '' )? '#'.$row->settings->er_bc_icon_color : 'inherit' ?>;
+					font-size: <?php echo ($row->settings->er_icon_size != '' )? $row->settings->er_icon_size.'px' : 'inherit' ?>;
     				vertical-align: middle;
+    				padding: 0 10px;
+    				-webkit-transition: all 0.3s ease-out;
+					-moz-transition: all 0.3s ease-out;
+					-ms-transition: all 0.3s ease-out;
+					-o-transition: all 0.3s ease-out;
+					transition: all 0.3s ease-out;
+				}
+				.fl-node-<?php echo $row->node; ?> .bber-image {
+					padding: 0 10px;
+				}
+				.fl-node-<?php echo $row->node; ?> .bb-er-row:hover .bber-icon {
+					color: <?php echo ($row->settings->er_bc_icon_hcolor != '' )? '#'.$row->settings->er_bc_icon_hcolor : 'inherit' ?>;
 				}
 				<?php if ( ! FLBuilderModel::is_builder_active() ): ?>
 					.fl-node-<?php echo $row->node; ?> .fl-row-content-wrap {
@@ -286,9 +325,13 @@ function bb_er_row_css( $css, $nodes, $global_settings ) {
 				.fl-node-<?php echo $row->node; ?> .bb-er-row {
 					width:100%;
 					color: #<?php echo ($row->settings->er_bc_title_color != '') ? $row->settings->er_bc_title_color : '000' ; ?>;
-					background-color:#<?php echo ($row->settings->er_bc_bg_color != '') ? $row->settings->er_bc_bg_color : 'c7c7c7' ; ?>;
-					font-family: <?php echo ($row->settings->er_title_typography['family'] != 'Default')? $row->settings->er_title_typography : 'inherit' ?>;
-					font-weight: <?php echo ($row->settings->er_title_typography['weight'] != 'default' ) ? $row->settings->er_title_typography : '500' ; ?>;
+					<?php if( $row->settings->er_bg_type == 'color'): ?>
+						background-color:#<?php echo ($row->settings->er_bc_bg_color != '') ? $row->settings->er_bc_bg_color : 'c7c7c7' ; ?>;
+					<?php elseif ($row->settings->er_bg_type == 'image' ): ?>
+						background-image: url(<?php echo $row->settings->er_bc_bg_image_src; ?>);
+					<?php endif ?>
+					font-family: <?php echo ($row->settings->er_title_typography['family'] != 'Default')? $row->settings->er_title_typography['family'] : 'inherit' ?>;
+					font-weight: <?php echo ($row->settings->er_title_typography['weight'] != 'default' ) ? $row->settings->er_title_typography['weight'] : '500' ; ?>;
 					font-size: <?php echo ($row->settings->er_font_size != '' ) ? $row->settings->er_font_size : '28' ; ?>px;
 					line-height: <?php echo ($row->settings->er_line_height != '' ) ? $row->settings->er_line_height : '32' ; ?>px;
 					text-align: <?php echo $row->settings->er_title_align; ?>;
@@ -302,9 +345,27 @@ function bb_er_row_css( $css, $nodes, $global_settings ) {
 					-o-transition: all 0.3s ease-out;
 					transition: all 0.3s ease-out;
 				}
+				.fl-node-<?php echo $row->node; ?> .bber-expanded {
+					color: <?php echo ($row->settings->er_ac_title_color != '') ? '#'.$row->settings->er_ac_title_color : 'inherit' ; ?>;
+					<?php if ($row->settings->er_bg_type == 'image' ): ?>
+						background-color: <?php echo ($row->settings->er_ac_bg_color != '') ? '#'.$row->settings->er_ac_bg_color : 'c7c7c7' ; ?>;
+					<?php endif ?>
+
+				}
 				.fl-node-<?php echo $row->node; ?> .bb-er-row:hover {
 					color: <?php echo ($row->settings->er_bc_title_hcolor != '') ? '#'.$row->settings->er_bc_title_hcolor : 'inherit' ; ?>;
-					background-color: <?php echo ($row->settings->er_bc_bg_hcolor != '') ? '#'.$row->settings->er_bc_bg_hcolor : 'inherit' ; ?>;
+					<?php if ($row->settings->er_bg_type == 'image' ): ?>
+						background-color: <?php echo ($row->settings->er_bc_bg_hcolor != '') ? '#'.$row->settings->er_bc_bg_hcolor : 'inherit' ; ?>;
+					<?php endif ?>
+				}
+				.fl-node-<?php echo $row->node; ?> .bber-expanded:hover {
+					color: <?php echo ($row->settings->er_ac_title_hcolor != '') ? '#'.$row->settings->er_ac_title_hcolor : 'inherit' ; ?>;
+					<?php if ($row->settings->er_bg_type == 'image' ): ?>
+						background-color: <?php echo ($row->settings->er_ac_bg_hcolor != '') ? '#'.$row->settings->er_ac_bg_hcolor : 'c7c7c7' ; ?>;
+					<?php endif ?>	
+				}
+				.fl-node-<?php echo $row->node; ?> .bber-image img{
+					width: <?php echo ( $row->settings->er_image_size != '' ) ? $row->settings->er_image_size : 'auto' ?>px;
 				}
 			<?php endif ?>
 		<?php
@@ -321,18 +382,42 @@ function bb_er_row_structure ( $js, $nodes, $global_settings ) {
 		?>
 		<?php if ( $row->settings->is_enable == 'yes' ): ?>
 			(function($) {
-				var html = '<div class="bb-er-row"><div><i class="bber-icon-top"></i></div><div class="bb-er-title-section"><span><i class="bber-icon-left"></i></span><span class="bb-er-title"><?php echo htmlspecialchars($row->settings->er_bc_title); ?></span><span><i class="bber-icon-right"></i></span><div><i class="bber-icon-bottom"></i></div></div></div>';
-				$('.fl-row.fl-node-<?php echo $row->node; ?>').prepend(html);
+				var html = '';				 
+				 html += '<div class="bb-er-row">';
+				 <?php if( $row->settings->er_icon_position == 'top' ): ?>
+				 	<?php if( $row->settings->er_img_type == 'icon' ): ?>
+				 		html += '<div><i class="bber-icon <?php echo $row->settings->er_bc_icon; ?>"></i></div>';
+				 	<?php elseif( $row->settings->er_img_type == 'image' ): ?>
+				 		html += '<div class="bber-image"><img src="<?php echo $row->settings->er_bc_image_src; ?>" /></div>';
+				 <?php endif ?>
+				<?php endif ?>
+				 html += '<div class="bb-er-title-section">';
+				<?php if( $row->settings->er_icon_position == 'left' ): ?>
+				 	<?php if( $row->settings->er_img_type == 'icon' ): ?>
+				 		html += '<span><i class="bber-icon <?php echo $row->settings->er_bc_icon; ?>"></i></span>';
+				 	<?php elseif( $row->settings->er_img_type == 'image' ): ?>
+				 		html += '<span class="bber-image"><img src="<?php echo $row->settings->er_bc_image_src; ?>" /></span>';
+				 <?php endif ?>
+				<?php endif ?>
+				 html += '<span class="bb-er-title"><?php echo htmlspecialchars($row->settings->er_bc_title); ?></span>';
+				<?php if( $row->settings->er_icon_position == 'right' ): ?>
+				 	<?php if( $row->settings->er_img_type == 'icon' ): ?>
+				 		html += '<span><i class="bber-icon <?php echo $row->settings->er_bc_icon; ?>"></i></span>';
+				 	<?php elseif( $row->settings->er_img_type == 'image' ): ?>
+				 		html += '<span class="bber-image"><img src="<?php echo $row->settings->er_bc_image_src; ?>" /></span>';
+				 <?php endif ?>
+				<?php endif ?>
+				 html += '</div>';
+				<?php if( $row->settings->er_icon_position == 'bottom' ): ?>
+				 	<?php if( $row->settings->er_img_type == 'icon' ): ?>
+				 		html += '<div><i class="bber-icon <?php echo $row->settings->er_bc_icon; ?>"></i></div>';
+				 	<?php elseif( $row->settings->er_img_type == 'image' ): ?>
+				 		html += '<div class="bber-image"><img src="<?php echo $row->settings->er_bc_image_src; ?>" /></div>';
+				 <?php endif ?>
+				<?php endif ?>
+				 html += '</div>';
 
-				<?php if($row->settings->er_icon_position == 'left'): ?>
-					$('.fl-row.fl-node-<?php echo $row->node; ?> .bber-icon-left').addClass('bber-icon <?php echo $row->settings->er_bc_icon; ?>');
-				<?php elseif ($row->settings->er_icon_position == 'right'): ?>
-					$('.fl-row.fl-node-<?php echo $row->node; ?> .bber-icon-right').addClass('bber-icon <?php echo $row->settings->er_bc_icon; ?>');
-				<?php elseif ($row->settings->er_icon_position == 'top'): ?>
-					$('.fl-row.fl-node-<?php echo $row->node; ?> .bber-icon-top').addClass('bber-icon <?php echo $row->settings->er_bc_icon; ?>');
-				<?php elseif ($row->settings->er_icon_position == 'bottom'): ?>
-					$('.fl-row.fl-node-<?php echo $row->node; ?> .bber-icon-bottom').addClass('bber-icon <?php echo $row->settings->er_bc_icon; ?>');
-				<?php endif?>
+				$('.fl-row.fl-node-<?php echo $row->node; ?>').prepend(html);
 
 				$('.fl-node-<?php echo $row->node; ?> .bb-er-row').click(function() {
 
@@ -352,6 +437,17 @@ function bb_er_row_structure ( $js, $nodes, $global_settings ) {
 						$('.fl-node-<?php echo $row->node; ?> .bber-icon').toggleClass("<?php echo $row->settings->er_bc_icon; ?> <?php echo $row->settings->er_ac_icon; ?>")
 					<?php endif ?>
 
+					// toggle background
+					<?php if ($row->settings->er_bg_type == 'image' ): ?>
+						var bg_toggle_src = ( $('.fl-node-<?php echo $row->node; ?> .bb-er-row').css('background-image') == 'url("<?php echo $row->settings->er_bc_bg_image_src; ?>")' ? 'url("<?php echo $row->settings->er_ac_bg_image_src; ?>")' : 'url("<?php echo $row->settings->er_bc_bg_image_src; ?>")' );
+						$('.fl-node-<?php echo $row->node; ?> .bb-er-row').css({'background-image' : bg_toggle_src });
+					<?php endif ?>
+
+					// toggle image icon
+					<?php if( $row->settings->er_img_type == 'image' ): ?>
+						var img_toggle_src = ( $('.fl-node-<?php echo $row->node; ?> .bber-image img').attr('src') == '<?php echo $row->settings->er_bc_image_src; ?>' ? '<?php echo $row->settings->er_ac_image_src; ?>' : '<?php echo $row->settings->er_bc_image_src; ?>' );
+						$('.fl-node-<?php echo $row->node; ?> .bber-image img').attr('src', img_toggle_src)
+					<?php endif ?>
 
 				});
 			})(jQuery);
